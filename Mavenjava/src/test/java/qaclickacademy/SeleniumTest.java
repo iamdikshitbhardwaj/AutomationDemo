b4f795bbb27927c3dbab2d109c6e1e75ea6ddb85
Dikshit <bhardwajfuture@gmail.com>
2019-05-29 18:18:48 +0530

    Changes in the methods using prioritization

--- Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -8,31 +8,53 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-	@Test
-	public void SeleniumMethod1() throws InterruptedException {
+
+	public WebDriver driver;
+	public String url = "https://www.google.com/";
+	public String googleSearchBar = ".//*[@name='q' and @type='text']";
+	public String secondIndexGoogleSuggestion = "//li[@role='presentation'][2]";
+	public String googleProductsButton = "//a[@title='Google apps']";
+
+	@BeforeTest
+	public void initialConfig() {
 		System.setProperty(
 				"webdriver.chrome.driver",
 				"C:\\Users\\52087137\\.m2\\repository\\org\\seleniumhq\\selenium\\selenium-chrome-driver\\3.141.5\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		String url = "https://www.google.com/";
-		driver.get(url);
-
-		// actions.contextClick().click().perform();
-		WebElement element = driver.findElement(By.xpath((".//*[@name='q' and @type='text']")));
-		element.sendKeys("Find xpath using selenium");
 	
-		WebDriverWait wait=new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@role='presentation'][2]")));
-		driver.findElement(By.xpath("//li[@role='presentation'][2]")).click();
+	}
+
+	@Test (priority=1)	
+	public void openBrowser(){
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);	
+		WebElement googleSearchText = driver.findElement(By.xpath((googleSearchBar)));
+		googleSearchText.sendKeys("Find xpath using selenium");
 
 	}
 
-	// @Test
-	public void login() {
+	@Test(priority=2)	
+	public void login(){
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(secondIndexGoogleSuggestion)));
+		driver.findElement(By.xpath(secondIndexGoogleSuggestion)).click();
+	}
 
+	 @Test(priority=3)	
+	 public void openApps() {
+		WebElement googleProducts = driver.findElement(By.xpath(googleProductsButton));
+		googleProducts.click();
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
 	}
 }
